acae9e93278f6b8027559be3e898876fdb8be6a2
wow132 <102208871+wow132@users.noreply.github.com>
2024-06-13 16:20:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 import streamlit as st
 from openai import OpenAI
 
+def draw(prompt):
+    response = client.images.generate(model="dall-e-3",prompt= prompt)
+    image_url = response.data[0].url
+    image = f"![alt text]({image_url})"
+    return image
+
 apikey = st.text_input("api key를 입력하세요", type="password")
+
+client = OpenAI(api_key=apikey)
 
 st.header("음식 메뉴 추천")
 prompti = st.text_input("키워드")
 
-@st.cache_data()
-def draw(prompt):
-    client = OpenAI(api_key=apikey)
-    response = client.images.generate(model="dall-e-3",prompt=f'{prompt}와 관련된 음식 메뉴 하나를 그려줘')
-    image_url = response.data[0].url
-    image = f"![alt text]({image_url})"
-    return image
-   
 if st.button("start"):
-  img = draw(prompti)
-  st.markdown(img)
+    response = client.chat.completions.create(
+      model="gpt-4o",
+      messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f'{prompti}와 관련된 음식 한가지를 추천해줘'},
+      ]
+    )
+    r = response.choices[0].message.content
+    st.markdown(r)
+    img = draw(r)
+    st.markdown(img)
